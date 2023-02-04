1654946c4dbb3effc3e6e0947bb0848f13765bc5
Asus <Asus@BEHZAD>
2023-02-04 14:34:47 +0330

    Add project files.

---
+++ InitializationOrder/Program.cs
@@ -0,0 +1,45 @@
+﻿var myObject = new Derived();
+
+class Base
+{
+    public Base()
+    {
+        _ = new Tracker("Base.Instance.Constructor");
+        this.Virtual();
+    }
+    static Base()
+    {
+        _ = new Tracker("Base.Static.Constructor");
+    }
+    private Tracker m_Field = new("Base.Instance.Field");
+    static private Tracker s_Field = new("Base.Static.Field");
+    virtual public void Virtual()
+    {
+        _ = new Tracker("Base.Instance.Virtual");
+    }
+}
+
+class Derived : Base
+{
+    public Derived()
+    {
+        _ = new Tracker("Derived.Instance.Constructor");
+    }
+    static Derived()
+    {
+        _ = new Tracker("Derived.Static.Constructor");
+    }
+    private Tracker m_Field = new("Derived.Instance.Field");
+    static private Tracker s_Field = new("Derived.Static.Field");
+    override public void Virtual()
+    {
+        _ = new Tracker("Derived.Instance.Virtual");
+    }
+}
+class Tracker
+{
+    public Tracker(string text)
+    {
+        Console.WriteLine(text);
+    }
+}
